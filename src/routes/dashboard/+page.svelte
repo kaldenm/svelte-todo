74b88d26a94f537e7bd7401f52d66c28cae19fea
kaldenm <kaldenmyers@gmail.com>
2024-11-13 17:46:57 -0600

    Creating a todo, issue with text binding

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import TodoForm from "../../components/dashboard/TodoForm.svelte"
+    import TodoList from "../../components/dashboard/TodoList.svelte"
+
+
+    // State 
+
+    let todoInput = ''
+
+
+    let todoList = [ 
+        {
+            text: "Buy more milk",
+            status: true
+
+        },
+        {
+            text: "Buy ramen",
+            status: false 
+        }
+    ]
+
+    function addToList() {
+        console.log(todoInput)
+        todoList = [
+            { text: todoInput, status: true },
+            ...todoList
+        ]
+    }
+
+</script>
+
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <TodoForm todoInput={todoInput} add={addToList}/>
+        <TodoList todos={todoList}/>
+    </div>
+</div>
